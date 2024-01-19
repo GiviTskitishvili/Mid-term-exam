0dea51a8dbe55bfc9fadd311537c0c7a3b511969
GiviTskitishvili <tgivi@yahoo.com>
2024-01-19 04:46:10 +0400

    add javascript

---
+++ script/header.js
@@ -0,0 +1,13 @@
+document.getElementById("btn-signup").addEventListener("click", function () {
+  window.location.href = "../pages/registration.html";
+});
+
+document
+  .getElementById("popularbutton1")
+  .addEventListener("click", function () {
+    window.location.href = "../pages/popular.html";
+  });
+
+document.getElementById("comerbutton1").addEventListener("click", function () {
+  window.location.href = "../pages/comer.html";
+});
